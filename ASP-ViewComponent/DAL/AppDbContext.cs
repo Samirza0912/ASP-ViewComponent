c20f6f2e1967f52ec17eff492809830875c55659
Samir Z. Amiraslanov <samirza@code.local>
2022-06-23 02:36:04 -0700

    feat:done

---
+++ ASP-ViewComponent/DAL/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ASP_ViewComponent.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_ViewComponent.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookAndGenreRelation> Relations { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<SocialAccount> SocialAccounts { get; set; } 
+        public DbSet<User> Users { get; set; }
+        
+       
+
+    }
+}
---
+++ ASP-ViewComponent/Models/BookAndGenreRelation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_ViewComponent.Models
+{
+    public class BookAndGenreRelation
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
